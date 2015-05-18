e7e1a0c49bf33f6a1186f33fcf5f8e5c1ec4d158
Alex Lu <alexlu@Alexs-MacBook-Pro.local>
2015-05-17 17:55:33 -0700

    started adding manifoldjs integration

--- app/nwapp/js/main.js
+++ app/nwapp/js/main.js
@@ -1,6 +1,18 @@
 var gui = require('nw.gui');
+var manifoldjs = require('manifoldjs');
+var manifestTools = manifoldjs.manifestTools;
+var projectBuilder = manifoldjs.projectBuilder;
 console.log(gui.App.argv);
 
 gui.App.on('open', function(file) {
   console.log(file);
+});
+
+console.log(process.cwd());
+var siteUrl = 'http://seksenov.github.io/ContosoTravel/';
+manifestTools.getManifestFromSite(siteUrl, function(err, response, body) {
+  console.log(err);
+  console.log(response);
+  var manifest = response;
+  projectBuilder.createWindows10App(manifest, 'hostedapp');
 });
