5db6d2c432163167114c5ef8d2fbf9998d053f55
Alex Lu <alexlu@Alexs-MacBook-Pro.local>
2015-05-19 00:13:32 -0700

    added install button and app info

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -3,12 +3,12 @@ var gui = require('nw.gui');
 var manifoldjs = require('manifoldjs');
 var manifestTools = manifoldjs.manifestTools;
 var projectBuilder = manifoldjs.projectBuilder;
+var projectTools = manifoldjs.projectTools;
 
 var HOSTED_APP_DIR = 'hostedapp';
-
+var originalPath = process.cwd();
 
 console.log(gui.App.argv);
-
 
 gui.App.on('open', function(file) {
   console.log(file);
@@ -21,8 +21,6 @@ function processUrl(url) {
   return url;
 }
 
-console.log(process.cwd());
-
 var siteUrl = processUrl('http://seksenov.github.io/ContosoTravel/');
 manifestTools.getManifestFromSite(siteUrl, function(err, response) {
   console.log(err);
@@ -32,16 +30,34 @@ manifestTools.getManifestFromSite(siteUrl, function(err, response) {
     manifest.content.start_url = processUrl(manifest.content.start_url);
   }
   var result = projectBuilder.createWindows10App(manifest, HOSTED_APP_DIR);
-  //removeSplash();
   result.then(function() {
-    var outputDir = process.cwd() + '/' + HOSTED_APP_DIR + '/windows10/manifest';
+    generateView(manifest.content);
     removeSplash();
     console.log('complete!');
   });
 });
 
+function generateView(app) {
+  var appTitle = document.getElementById('app-name');
+  var appIcon = document.getElementById('app-icon');
+  var installBtn = document.getElementById('install-btn');
+  appTitle.innerText = app.name;
+  appIcon.src = app.icons[0].src;
+  installBtn.addEventListener('click', function() {
+    var outputDir = process.cwd() + '/' + HOSTED_APP_DIR + '/windows10/manifest';
+    process.chdir(outputDir);
+    console.log(process.cwd());
+    projectTools.runApp('windows10', function(err) {
+      if (err) {
+        console.log(err);
+      }
+      process.chdir(originalPath);
+      console.log(process.cwd());
+    });
+  }, false);
+}
+
 function removeSplash() {
-  console.log('test removeSplash');
   setTimeout(function() {
     var splash = document.getElementById('splash');
     splash.className='animated fadeOut';
